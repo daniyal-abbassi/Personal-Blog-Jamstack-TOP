b7c463fd56604f3e0e20c00c4b9711853b17314b
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-16 03:28:34 +0330

    chore: handle login controller

---
+++ controllers/loginController.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const dbClient = require('../db/dbClient');
+
+const loginController = {
+    loginPost: async(req,res)=>{
+        try {
+            const {username,password} = req.body;
+            const existingUser = await dbClient.searchUser(username);
+            if(!existingUser) {
+                res.status(401).send('User Not exists!!!')
+            } else {
+                const matchPass = await bcrypt.compare(password,existingUser.password);
+                if(matchPass) {
+                    const payload = {userId: existingUser.user_id,username: existingUser.username}
+                   jwt.sign(payload,process.env.JWT_SECRET,{expiresIn: '1d'},(err,token)=>{
+                    res.json({token})
+                   })
+                   //console.log the user to test 
+                   console.log('user is: ',existingUser)
+                } else {
+                    res.status(401).send('Invalid credentials')
+                }
+            }
+        } catch (error) {
+            console.error('Server error!',error);
+            res.status(500).send('Server error log-in',error);
+        }
+    }
+}
+
+
+
+module.exports=loginController;
--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -1,10 +1,16 @@
 const {Router} = require('express');
 const loginRouter = Router();
+const loginController = require('../controllers/loginController');
+
+
+
+
 
 loginRouter.get('/',(req,res)=>{
     res.render('login')
 })
 
+loginRouter.post('/',loginController.loginPost)
 
 
 
