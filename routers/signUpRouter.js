0cfe059a2edd64b88275c26d5a5a08adfb5431ec
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-16 02:10:46 +0330

    chore: setup signup router

--- routers/signUpRouter.js
+++ routers/signUpRouter.js
@@ -1,8 +1,11 @@
 const {Router} = require('express')
 const signupRouter = Router();
+const dbClient = require('../db/dbClient');
+signupRouter.get('/',(req,res)=>res.render('signup'))
 
-
-
-
+signupRouter.post('/',(req,res)=>{
+    const {username,password} = req.body;
+    
+})
 
 module.exports=signupRouter;
