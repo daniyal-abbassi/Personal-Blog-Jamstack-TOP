6c27d2d0af709126156c0549559005234ad1f74a
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-16 20:42:53 +0330

    chore: test saving post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -4,10 +4,10 @@ const postsController = {
     createPost: async(req,res)=>{
         const {title,content,published} = req.body;
         const {userId} = req.user;
-        console.log('this should be user: ',userId)
         try {
-            // const post = await dbClient.createPost()
             console.log(title,content,published,userId)
+            const post = await dbClient.createPost(title,content,'published',userId);
+            console.log('this is post: ',post)
         } catch (error) {
             res.status(500).json({message: 'error in creating  post!!!'})
         }
