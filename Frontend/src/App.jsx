4bc7f4caa425f2889cf552cfe44f8aa68d21b51e
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-19 01:06:47 +0330

    chore: pass authentication state to routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Blog from "./components/pages/Blog";
 import SignUp from "./components/pages/SignUp";
 import SignIn from "./components/pages/SignIn";
+import { useState } from "react";
 
 export default function App() {
+    const [isAuthenticated,setIsAuthenticated] = useState(false);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Blog />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/" element={<Blog isAuthenticated={isAuthenticated}/>} />
+        <Route path="/sign-up" element={<SignUp onAuthChange={setIsAuthenticated}/>} />
+        <Route path="/sign-in" element={<SignIn onAuthChange={setIsAuthenticated}/>} />
       </Routes>
     </BrowserRouter>
   );
