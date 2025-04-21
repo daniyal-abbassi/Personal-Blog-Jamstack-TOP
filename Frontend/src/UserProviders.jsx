538d582a334a8c962bbc4a317331e34c49d4f66c
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-22 01:21:38 +0330

    chore: adopt userProvider with useUser hook

--- Frontend/src/UserProviders.jsx
+++ Frontend/src/UserProviders.jsx
@@ -1,15 +1,20 @@
 import { createContext } from "react";
 import PropTypes from "prop-types";
-import useUser from "@/hooks/useUser.js";
+import useUser from "./hooks/useUser";
 
 // Create Context
 export const UserContext = createContext({
   user: null,
+  setUser: () => {},
+  isAuthenticated: false,
+  setIsAuthenticated: () => {},
+  loading: true, // Add loading to default value
+  error: null,   // Add error to default value
 });
 
 // Custom Provider
 const MyProvider = ({ children }) => {
-  const { loading, error, user, setUser } = useUser();
+  const { loading, error, user, setUser, isAuthenticated, setIsAuthenticated } = useUser();
 
   return (
     <UserContext.Provider value={{ user, setUser, loading, error }}>
