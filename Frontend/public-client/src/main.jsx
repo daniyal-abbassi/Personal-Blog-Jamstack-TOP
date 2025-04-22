b75004eac6f8da5faf98a64af5092f649d92322d
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-23 02:54:26 +0330

    chore: delete userprovider

--- Frontend/public-client/src/main.jsx
+++ Frontend/public-client/src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { MyProvider as UserProvider } from './UserProviders';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <UserProvider>
     <App />
-    </UserProvider>
   </StrictMode>
 );
