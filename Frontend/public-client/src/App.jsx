74294d4a294caebfb4a5453294ce8453f1cf2c65
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-23 02:56:10 +0330

    chore: delete context from public clinet

--- Frontend/public-client/src/App.jsx
+++ Frontend/public-client/src/App.jsx
@@ -2,28 +2,15 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Blog from "./pages/Blog";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
-import Admin from "../../private-client/src/pages/Admin/Admin";
-import { useContext} from "react";
-import { UserContext } from "../../private-client/src/UserProviders";
-import {ThemeProvider} from "../../private-client/src/components/ThemeProvider";
-import { Toaster } from "./components/ui/toaster";
 export default function App() {
-    const {isAuthenticated} = useContext(UserContext);
 
     
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Blog isAuthenticated={isAuthenticated}/>} />
+        <Route path="/" element={<Blog/>} />
         <Route path="/sign-up" element={<SignUp/>} />
         <Route path="/sign-in" element={<SignIn/>} />
-        <Route path="/admin" element={
-          <ThemeProvider>
-            <Admin />
-            <Toaster />
-          </ThemeProvider>
-          
-          } />
       </Routes>
     </BrowserRouter>
   );
