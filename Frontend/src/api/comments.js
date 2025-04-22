f3e0ee268bd5ad2f29758c82bc27262f5bc7b852
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-22 22:49:39 +0330

    feat: add comments api file

---
+++ Frontend/src/api/comments.js
@@ -0,0 +1,32 @@
+const API_BASE_URL = 'http://localhost:3001/api';
+
+
+export const getComments = async(postId) =>{
+    try {
+        const response = await fetch(`${API_BASE_URL}/posts/${postId}`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        return await response.json();
+    } catch (error) {
+        console.error('ERROR FETCHING COMMENTS: ',error);
+        throw error
+    }
+};
+
+export const deleteComment = async(postId,commentId) => {
+    try {
+        const response = await fetch(`${API_BASE_URL}/posts/${postId}/comments/${commentId}`,{
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        return await response.json();
+    } catch (error) {
+        console.error('ERROR FETCHING COMMENTS: ',error)
+        throw error;
+    }
+}
