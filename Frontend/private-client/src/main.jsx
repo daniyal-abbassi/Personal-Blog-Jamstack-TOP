273b18dda33c2fd0b98af661d1249e40d8e103d4
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-23 20:40:13 +0330

    style: add index and tailwindcss.config

--- Frontend/private-client/src/main.jsx
+++ Frontend/private-client/src/main.jsx
@@ -5,6 +5,7 @@ import { Toaster } from "./components/ui/toaster";
 import Admin from "./pages/Admin/Admin";
 import { BrowserRouter } from "react-router-dom";
 import { MyProvider as UserProvider } from "./UserProviders";
+import './index.css';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
