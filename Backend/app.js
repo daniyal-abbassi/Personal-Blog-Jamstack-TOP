a51ab6dff192ab0d2edc08d28dc9c723ac4716ec
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-23 05:28:02 +0330

    fix: add admin port

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','http://localhost:5174'],
     credentials: true
 }
 app.use(cors(corsOptions))
