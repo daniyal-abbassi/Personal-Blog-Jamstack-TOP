425d0719cc77f74a449816159ab4b8d8e18618a3
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-27 01:39:33 +0330

    chore: add usePosts hook for public

---
+++ Frontend/public-client/src/hooks/usePosts.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { getPosts } from "../api/posts";
+
+const usePosts = (sortValue = "created_at", order = "asc", search = "") => {
+  const [posts, setPosts] = useState([]);
+  const [postsLoading, setPostsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const queries = new URLSearchParams({
+    sortValue,
+    order,
+    search,
+  }).toString();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const data = await getPosts(queries);
+        setPosts(data);
+      } catch (error) {
+        setError(error);
+        setPosts([])
+      } finally {
+        setPostsLoading(false);
+      }
+    };
+    fetchPosts();
+
+  }, [queries]);
+
+  return { postsLoading, error, posts, setPosts };
+};
+
+export default usePosts;
