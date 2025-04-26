04161696a7f35388b7ccd2ec2e2304e01f131fbb
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-27 03:09:53 +0330

    feat: add cloudinary config

---
+++ Backend/utils/cloudinaryConfig.js
@@ -0,0 +1,8 @@
+const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
+
+cloudinary.config({
+    cloud_name: 'dpnpuufpd',
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET
+})
