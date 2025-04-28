1b86d72aa33bc6a9f33c3cb6951e7905078daa2b
daniyal-abbassi <slimshady3753@protonmail.com>
2025-04-28 22:32:56 +0330

    chore: install popover and command ui

--- Frontend/private-client/src/pages/Admin/CreatePost/CreatePostForm.jsx
+++ Frontend/private-client/src/pages/Admin/CreatePost/CreatePostForm.jsx
@@ -63,6 +63,25 @@ function CreatePostForm({ setPosts, switchTab }) {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 ">
+      <FormField
+          control={form.control}
+          name="tag"
+          render={({ field }) => (
+            <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+              <FormControl>
+                <Checkbox
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                />
+              </FormControl>
+              <div className="space-y-1 leading-none">
+                <FormLabel>new tag</FormLabel>
+                <FormMessage />
+              </div>
+              
+            </FormItem>
+          )}
+        />
         <FormField
           control={form.control}
           name="title"
