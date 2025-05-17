8ca7a53c529c79b826058c745d0e19a0abdf0f29
daniyal-abbassi <slimshady3753@protonmail.com>
2025-05-17 20:04:11 +0330

    feat: add api call for single post

--- Frontend/public-client/src/api/posts.js
+++ Frontend/public-client/src/api/posts.js
@@ -22,3 +22,24 @@ export const getPosts = async(queries = '') => {
         throw error;
     }
 }
+
+//GET A SINGLE POST
+export const getPost = async(postId) => {
+    try {
+        const response = await fetch(`${API_BASE_URL}/posts/post/:${postId}`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        if(!response.ok) {
+            const errorData = await response.json();
+            throw new Error('ERROR TO FETCH POST',response.status)
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('ERROR FETCH POSTS',error);
+        throw error;
+    }
+}
